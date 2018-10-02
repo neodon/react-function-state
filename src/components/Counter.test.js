101bd12df4ed04af99cffd93dc77fd70a3efabc0
James Barton <james@neodon.com>
2018-10-02 18:34:45 -0500

    test handleMultiplyByFive

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactTestUtils from 'react-dom/test-utils'
 import Counter from './Counter'
 
 const initialCounterValue = 0
@@ -20,5 +21,13 @@ describe('Counter', () => {
     expect(div.querySelector('h1').textContent).toBe(initialCounterValue.toString())
     counter.setState({ value: 42 })
     expect(div.querySelector('h1').textContent).toBe("42")
+  })
+
+  it('handleMultiplyByFive multiplies the value by 5', () => {
+    counter.setState({ value: 42 })
+    ReactTestUtils.Simulate.click(div.querySelectorAll('button')[0])
+    expect(div.querySelector('h1').textContent).toBe((42 * 5).toString())
+    ReactTestUtils.Simulate.click(div.querySelectorAll('button')[0])
+    expect(div.querySelector('h1').textContent).toBe((42 * 5 * 5).toString())
   })
 })
